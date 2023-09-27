e188a577b9bb2172bf8d7a2dad5e1c5a91b96803
lobato123 <felipepan64@gmail.com>
2023-09-27 17:54:56 -0300

    GerenciamentoEscola

---
+++ app/src/main/java/com/example/gerenciamentoescola/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.gerenciamentoescola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+//    public void telaCadastrar(View view) {
+//        Intent intent = new Intent(MainActivity.this, telaCadastrar.class);
+//        startActivity(intent);
+//    }
+}
+
---
+++ app/src/main/java/com/example/gerenciamentoescola/telaConsultar.kt
@@ -0,0 +1,11 @@
+package com.example.gerenciamentoescola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class telaConsultar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_consultar)
+    }
+}
